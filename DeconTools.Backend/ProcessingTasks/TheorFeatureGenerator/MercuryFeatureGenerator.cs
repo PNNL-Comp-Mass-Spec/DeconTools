9343a79b262ea417d856c2dc0d07726d43d599cb
Gordon Slysz <gordon.slysz@gmail.com>
2010-01-27 19:59:51 +0000

    added TheorFeatureGenerator/MercuryFeatureGenerator.cs and TheorXYDataCalculationUtilities.cs

---
+++ DeconTools.Backend/ProcessingTasks/TheorFeatureGenerator/MercuryFeatureGenerator.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DeconTools.Utilities;
+using DeconTools.Backend.Core;
+using DeconTools.Backend.Utilities;
+using DeconTools.Backend.ProcessingTasks.FitScoreCalculators;
+
+namespace DeconTools.Backend.ProcessingTasks.TheorFeatureGenerator
+{
+    public class MercuryFeatureGenerator : ITheorFeatureGenerator
+    {
+
+        MercuryDistributionCreator distributionCreator;
+
+        #region Constructors
+        public MercuryFeatureGenerator()
+        {
+            this.Name = "MercuryFeatureGenerator";
+        }
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Public Methods
+        #endregion
+
+        #region Private Methods
+        #endregion
+        public override void GenerateTheorFeature(DeconTools.Backend.Core.ResultCollection results)
+        {
+            Check.Require(results.Run.CurrentMassTag != null, this.Name + " failed; CurrentMassTag hasn't been declared");
+            //Check.Require(results.Run.CurrentMassTag.EmpiricalFormula!=null, this.Name + "failed; Problem with EmpiricalFormular of current mass tag.");
+
+            MassTag mt = results.Run.CurrentMassTag;
+
+            IMassTagResult result = results.GetMassTagResult(mt);
+
+            Check.Require(result != null, this.Name + " failed; This task requires a MassTagResult, which is null");
+            double fwhm = result.IsotopicProfile.GetFWHM();
+
+
+            XYData theorXYData = new XYData();
+            for (int i = 0; i < mt.IsotopicProfile.Peaklist.Count; i++)
+            {
+
+                XYData peakXYData = getTheorPeakData(mt.IsotopicProfile.Peaklist[i], fwhm);
+
+
+
+
+            }
+
+
+            distributionCreator.MolecularFormula = new MolecularFormula();
+            distributionCreator.getIsotopicProfile();
+            distributionCreator.OffsetDistribution(result.IsotopicProfile);
+
+            AreaFitter areafitter = new AreaFitter(distributionCreator.Data, result.Run.XYData, 10);
+            double fitval = areafitter.getFit();
+
+            if (fitval == double.NaN || fitval > 1) fitval = 1;
+
+            result.IsotopicProfile.Score = fitval;
+
+        }
+
+        private XYData getTheorPeakData(MSPeak mSPeak, double fwhm)
+        {
+            throw new NotImplementedException();
+        }
+
+  
+    }
+}
---
+++ DeconTools.Backend/Utilities/IsotopeDistributionCalculation/TheorXYDataCalculationUtilities.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DeconTools.Backend.Core;
+using DeconTools.Utilities;
+
+namespace DeconTools.Backend.Utilities.IsotopeDistributionCalculation
+{
+    public class TheorXYDataCalculationUtilities
+    {
+        #region Constructors
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Public Methods
+
+        //TODO:  make this more generic!  doesn't need to be linked to IPeak or fwhm.... 
+        public static XYData getTheorPeakData(IPeak peak, double fwhm)
+        {
+            XYData xydata = new XYData();
+            double one_over_sqrt_of_2_pi = 0.3989423;
+            double sigma = fwhm / 2.35;      //   width@half-height =  2.35σ   (Gaussian peak theory)
+            double centerXVal = peak.XValue;
+
+            double sixsigma = 3 * fwhm;
+
+
+            int numPoints = 201;
+            double mz_per_point = sixsigma / (double)(numPoints - 1);
+
+            int startPoint = 0 - (numPoints - 1) / 2;
+            int stopPoint = 0 + (numPoints - 1) / 2;
+
+            xydata.Xvalues = new double[numPoints];
+            xydata.Yvalues = new double[numPoints];
+
+            int counter = 0;
+            for (int i = startPoint; i <= stopPoint; i++)
+            {
+                double mz = centerXVal + mz_per_point * (i);
+                double intens = (1 / sigma) * one_over_sqrt_of_2_pi * Math.Exp(-1 * ((mz - centerXVal) * (mz - centerXVal)) / (2 * sigma * sigma));
+
+                xydata.Xvalues[counter] = mz;
+                xydata.Yvalues[counter] = intens;
+                counter++;
+            }
+
+            xydata.NormalizeYData();
+
+
+            for (int i = 0; i < xydata.Yvalues.Length; i++)
+            {
+                xydata.Yvalues[i] = xydata.Yvalues[i] * peak.Height;
+            }
+            return xydata;
+        }
+
+        public static XYData Get_Theoretical_IsotopicProfileXYData(IsotopicProfile isotopicProfile, double fwhm)
+        {
+            Check.Require(isotopicProfile != null && isotopicProfile.Peaklist != null &&
+            isotopicProfile.Peaklist.Count > 0, "Cannot get theor isotopic profile. Input isotopic profile is empty.");
+
+            XYData xydata = new XYData();
+            List<double> xvals = new List<double>();
+            List<double> yvals = new List<double>();
+
+            for (int i = 0; i < isotopicProfile.Peaklist.Count; i++)
+            {
+                XYData tempXYData = getTheorPeakData(isotopicProfile.Peaklist[i], fwhm);
+                xvals.AddRange(tempXYData.Xvalues.ToList());
+                yvals.AddRange(tempXYData.Yvalues.ToList());
+
+            }
+            xydata.Xvalues = xvals.ToArray();
+            xydata.Yvalues = yvals.ToArray();
+
+            
+
+            return xydata;
+        }
+
+
+
+        #endregion
+
+        #region Private Methods
+        #endregion
+
+        public static XYData Get_Theoretical_IsotopicProfileXYData(IsotopicProfile theorProfile, double fwhm, double mzOffset)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
