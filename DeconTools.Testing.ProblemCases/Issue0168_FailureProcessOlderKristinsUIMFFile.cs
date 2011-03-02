4605004301592d9cfa48dd20bcbb5b340c8ce2ac
Gordon Slysz <gordon.slysz@gmail.com>
2011-03-02 18:43:05 +0000

    added code for testing redmine issue 0168

---
+++ DeconTools.Testing.ProblemCases/Issue0168_FailureProcessOlderKristinsUIMFFile.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using DeconTools.Backend.Runs;
+
+namespace DeconTools.Testing.ProblemCases
+{
+    [TestFixture]
+    public class Issue0168_FailureProcessOlderKristinsUIMFFile
+    {
+        string testFile = @"D:\Data\Redmine_issues\Issue0168_FailureOnKristinsOldUIMFFiles\Dey_KO_8721_02_17Nov10_10-09-23_0000.UIMF";
+
+
+        string standardFile = @"\\protoapps\UserData\Slysz\DeconTools_TestFiles\35min_QC_Shew_Formic_4T_1.8_500_20_30ms_fr1950_0000.uimf";
+
+
+        [Test]
+        public void getMassSpectra()
+        {
+            RunFactory runfactory = new RunFactory();
+
+            UIMFRun run = (UIMFRun)runfactory.CreateRun(testFile);
+
+            UIMFRunTester tester = new UIMFRunTester();
+
+            int startScan = run.MinScan;
+            int stopScan = run.MaxScan;
+
+            int startFrame = 6;
+            int stopFrame = 6;
+
+
+            tester.DisplayBasicRunInfo(run);
+            tester.GenerateMassSpectraForRanges(run, startFrame, stopFrame, startScan, stopScan);
+
+           // tester.DisplayFrameParameters(run, startFrame, stopFrame);
+
+
+        }
+
+        [Test]
+        public void isolatedProblem_Test1()
+        {
+            RunFactory runfactory = new RunFactory();
+
+            UIMFRun run = (UIMFRun)runfactory.CreateRun(testFile);
+
+            UIMFRunTester tester = new UIMFRunTester();
+
+            int startScan = 283;
+            int stopScan = 286;
+
+            int startFrame = 6;
+            int stopFrame = 6;
+
+
+            tester.DisplayBasicRunInfo(run);
+            tester.GenerateMassSpectraForRanges(run, startFrame, stopFrame, startScan, stopScan);
+
+            // tester.DisplayFrameParameters(run, startFrame, stopFrame);
+
+
+        }
+
+
+        [Test]
+        public void isolatedProblem_Test2()
+        {
+            UIMFLibrary.DataReader dr = new UIMFLibrary.DataReader();
+            dr.OpenUIMF(testFile);
+
+            UIMFLibrary.GlobalParameters gp=dr.GetGlobalParameters();
+
+            int numBins= gp.Bins;
+
+            double[] mzs = new double[numBins];
+            int[]intensities = new int[numBins];
+
+            int startFrame = 6;
+            int stopFrame = 6;
+
+
+            //the following throws an error
+            dr.SumScansNonCached(mzs, intensities, 0, startFrame, stopFrame, 285, 285);
+
+            // tester.DisplayFrameParameters(run, startFrame, stopFrame);
+
+
+        }
+
+
+
+        [Test]
+        public void displayFrameInfo()
+        {
+            RunFactory runfactory = new RunFactory();
+
+            UIMFRun run = (UIMFRun)runfactory.CreateRun(testFile);
+
+            UIMFRunTester tester = new UIMFRunTester();
+
+            int startScan = run.MinScan;
+            int stopScan = run.MaxScan;
+
+            int startFrame = 1;
+            int stopFrame = 7;
+
+
+            tester.DisplayBasicRunInfo(run);
+            
+            tester.DisplayFrameParameters(run, startFrame, stopFrame);
+
+
+        }
+
+
+        [Test]
+        public void test_usingStandardFile()
+        {
+            RunFactory runfactory = new RunFactory();
+
+            UIMFRun run = (UIMFRun)runfactory.CreateRun(standardFile);
+
+            UIMFRunTester tester = new UIMFRunTester();
+
+            int startScan = run.MinScan;
+            int stopScan = run.MaxScan;
+
+            int startFrame = 1;
+            int stopFrame = 10;
+
+
+            tester.DisplayBasicRunInfo(run);
+            tester.GenerateMassSpectraForRanges(run, startFrame, stopFrame, startScan, stopScan);
+
+
+        }
+
+
+    }
+}
---
+++ DeconTools.Testing.ProblemCases/UIMFRunTester.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DeconTools.Backend.Core;
+using DeconTools.Backend.Utilities;
+using DeconTools.Backend.ProcessingTasks;
+using UIMFLibrary;
+using DeconTools.Backend.Runs;
+
+namespace DeconTools.Testing.ProblemCases
+{
+    public class UIMFRunTester
+    {
+        public void DisplayBasicRunInfo(Backend.Runs.UIMFRun run)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("field\tvalue\n");
+            sb.Append("filename\t" + run.Filename + "\n");
+            sb.Append("filetype\t" + run.MSFileType + "\n");
+            sb.Append("minFrame\t" + run.MinFrame + "\n");
+            sb.Append("maxFrame\t" + run.MaxFrame + "\n");
+            sb.Append("minScan\t" + run.MinScan + "\n");
+            sb.Append("maxScan\t" + run.MaxScan + "\n");
+            Console.WriteLine(sb.ToString());
+
+        }
+
+
+
+        public void GenerateMassSpectraForRanges(Backend.Runs.UIMFRun run, int startFrame, int stopFrame, int startScan, int stopScan)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            FrameSetCollectionCreator fscc = new FrameSetCollectionCreator(run, startFrame, stopFrame, 1, 1);
+            fscc.Create();
+
+            ScanSetCollectionCreator sscc = new ScanSetCollectionCreator(run, startScan, stopScan, 1, 1);
+            sscc.Create();
+
+            UIMF_MSGenerator msgen = new UIMF_MSGenerator();
+
+
+            foreach (var frame in run.FrameSetCollection.FrameSetList)
+            {
+
+                run.CurrentFrameSet = frame;
+
+                foreach (var scan in run.ScanSetCollection.ScanSetList)
+                {
+                    run.CurrentScanSet = scan;
+
+                    sb.Append(frame.PrimaryFrame);
+                    sb.Append("\t");
+
+                    sb.Append(scan.PrimaryScanNumber);
+                    sb.Append("\t");
+                    try
+                    {
+                        msgen.Execute(run.ResultCollection);
+
+                        sb.Append("Passed");
+
+                    }
+                    catch (Exception ex)
+                    {
+                        sb.Append("FAILED. \t" + ex.Message);
+                    }
+
+                    sb.Append(Environment.NewLine);
+
+
+
+                }
+
+
+
+            }
+            Console.WriteLine(sb.ToString());
+
+
+
+
+
+
+
+        }
+
+        public void DisplayFrameParameters(UIMFRun run, int startFrame, int stopFrame)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            UIMFLibrary.DataReader dr = new DataReader();
+            dr.OpenUIMF(run.Filename);
+
+
+            FrameParameters fp;
+
+            sb.Append("frame\tnumScans\tpressureFront\tpressureBack\tTOFLength\tCalIntercept\tCalSlope\tFrameNum\tFrameType\n");
+
+            for (int frame = startFrame; frame <= stopFrame; frame++)
+            {
+                sb.Append(frame);
+                sb.Append("\t");
+
+
+                fp = dr.GetFrameParameters(frame);
+
+                sb.Append(fp.Scans);
+                sb.Append("\t");
+
+                sb.Append(fp.PressureFront);
+                sb.Append("\t");
+                sb.Append(fp.PressureBack);
+                sb.Append("\t");
+
+                sb.Append(fp.AverageTOFLength);
+                sb.Append("\t");
+                sb.Append(fp.CalibrationIntercept);
+                sb.Append("\t");
+                sb.Append(fp.CalibrationSlope);
+                sb.Append("\t");
+
+                sb.Append(fp.FrameNum);
+                sb.Append("\t");
+                sb.Append(fp.FrameType);
+                sb.Append(Environment.NewLine);
+
+
+
+
+            }
+
+            Console.WriteLine(sb.ToString());
+
+
+
+
+
+        }
+    }
+}
