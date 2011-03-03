90532c7fc1ce8331eaeecf90fa8832cbdf539cb7
Anuj Shah <anuj.shah@pnnl.gov>
2011-03-03 23:44:28 +0000

    Made the MSPeakResult implement IPeak

--- DeconTools.Backend/DTO/MSPeakResult.cs
+++ DeconTools.Backend/DTO/MSPeakResult.cs
@@ -6,10 +6,8 @@ using DeconTools.Backend.Core;
 
 namespace DeconTools.Backend.DTO
 {
-    public class MSPeakResult
+    public class MSPeakResult:IPeak
     {
-
-
         public MSPeakResult()
         {
             this.ChromID = -1;
@@ -33,6 +31,29 @@ namespace DeconTools.Backend.DTO
             this.Frame_num = frameNum;
         }
 
+        public override float Width
+        {
+            get
+            {
+                return this.MSPeak.Width;
+            }
+            set
+            {
+                this.MSPeak.Width = value;                     
+            }
+        }
+        public override double XValue
+        {
+            get { return this.MSPeak.XValue; }
+            set { this.MSPeak.XValue = value; }
+        }
+        
+        public override float Height
+        {
+            get { return this.MSPeak.Height; }
+            set { this.MSPeak.Height = value; }
+        }
+
         public int PeakID { get; set; }
         public int Scan_num { get; set; }
 
@@ -46,6 +67,42 @@ namespace DeconTools.Backend.DTO
 
         public MSPeak MSPeak { get; set; }
 
+        #region IComparable Members
+/*
+        public override int CompareTo(object obj)
+        {
+            if (obj == null)
+            {
+                return -1;
+            }
 
+
+            IPeak secondPeak = obj as IPeak;
+            if (secondPeak == null)
+            {
+                return -1;
+            }
+            else
+            {
+                
+                //TODO:: two peaks are the same if they are within a tolerance of each other in
+                //terms of mz, scan and lc frame. in this case we're only implementing mz values
+                //
+                double toleranceInPPM = 20;
+                double differenceInPPM = Math.Abs(1000000 * (secondPeak.XValue - this.XValue) / this.XValue);
+
+                if (differenceInPPM <= toleranceInPPM)
+                {
+                    return 0;
+                }
+
+            }
+
+            return 1;
+
+
+        }*/
+
+        #endregion
     }
 }
---
+++ DeconTools.Backend/DTO/MSResultPeakWithLocation.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DeconTools.Backend.Data.Structures;
+using DeconTools.Backend.Core;
+
+namespace DeconTools.Backend.DTO
+{
+    public class MSResultPeakWithLocation : IPeak
+    {
+
+        private ushort frameNumber;
+        private ushort scanNumber;
+        private Dictionary<ushort, List<ushort>> frameAndScansRange;
+
+        public MSResultPeakWithLocation(MSPeakResult peak)
+        {
+            this.XValue = peak.MSPeak.XValue;
+            this.frameNumber = (ushort) peak.Frame_num;
+            this.scanNumber = (ushort) peak.Scan_num;
+        }
+
+        /*
+        public MSResultPeakWithLocation(MSPeakResult peak, int numFrames, int numScans)
+        {
+            this.XValue = peak.MSPeak.XValue;
+            this.frameNumber = peak.Frame_num;
+            this.scanNumber = peak.Scan_num;
+
+            frameScansRange = new BitArray2D(numScans, numFrames);
+        }*/
+
+
+        /**
+         * constructor 
+         * Creates a peak result with a sorted list of framenumbers and scannumbers within those frame numbers
+         * 
+         * To improve efficiency these arrays should be passed in sorted order. We might be able to do that by
+         * using insertion sort during the creation of these arrays. 
+         * 
+         * An nlogn sort here might be an overkill.
+         * 
+         * */
+
+        public MSResultPeakWithLocation(MSPeak peak, Dictionary<ushort, List<ushort>> frameAndScansRange, int frameNum, int scanNum)
+        {
+            this.XValue = peak.XValue;
+            this.frameAndScansRange = frameAndScansRange;
+            this.frameNumber = (ushort)frameNum;
+            this.scanNumber = (ushort) scanNum;
+        }
+
+        /**
+        public MSResultPeakWithLocation(MSPeak peak, BitArray2D framesAndScans, int frame, int scan)
+        {
+            this.XValue = peak.XValue;
+            this.frameNumber = frame;
+            this.scanNumber = scan;
+            this.frameScansRange = framesAndScans;
+        }*/
+
+        private double mz;
+
+        public override double XValue
+        {
+            get { return mz; }
+            set { mz = value; }
+        }
+
+        private float intensity;
+        public override float Height
+        {
+            get { return intensity; }
+            set { intensity = value; }
+        }
+
+        private float fwhm;
+        public override float Width
+        {
+            get { return fwhm; }
+            set { fwhm = value; }
+        }
+
+
+        public override int CompareTo(object obj)
+        {
+            IPeak secondPeak = obj as IPeak;
+            if (secondPeak == null)
+            {
+                return -1;
+            }
+            else
+            {
+
+                   //we need a system level global parameter that is the tolerance in PPM
+                   //TODO
+                    double toleranceInPPM = 20;
+                    double differenceInPPM = Math.Abs(1000000 * (secondPeak.XValue - this.XValue) / this.XValue);
+
+                    if (differenceInPPM <= toleranceInPPM)
+                    {
+                        return 0;
+                    }
+                    else
+                    {
+                        return this.XValue.CompareTo(secondPeak.XValue);
+                    }
+
+            }
+
+        }
+
+        /*
+        public void updateFrameScansRange(BitArray2D frameScansFound)
+        {
+            this.frameScansRange.Or(frameScansFound);
+
+        }
+        */
+        private List<ushort> mergeSortUnique(List<ushort> list1, List<ushort> list2)
+        {
+            Dictionary<ushort, ushort> valueMap = new Dictionary<ushort, ushort>();
+            int end1 = list1.Count;
+            int end2 = list2.Count;
+
+            int index1 = 0, index2 = 0;
+            List<ushort> mergedArray = new List<ushort>();
+            while (index1 < end1 && index2 < end2)
+            {
+
+                if (list1[index1] < list2[index2])
+                {
+                    if (!valueMap.ContainsKey(list1[index1])){
+                        valueMap.Add(list1[index1], 1);
+                        mergedArray.Add(list1[index1]);
+                    }
+                    index1++;
+                }
+                else
+                {
+                    if (!valueMap.ContainsKey(list2[index2]))
+                    {
+                        valueMap.Add(list2[index2], 1);
+                        mergedArray.Add(list2[index2]);
+                        
+                    }
+                    index2++;
+                }
+            }
+
+            //find out which array is remaining to be appended and simply append that
+            if (index1 == end1)
+            {
+                //then array 2 is remaining
+                for (int i = index2; i < end2; i++)
+                {
+                    if (!valueMap.ContainsKey(list2[i]))
+                    {
+                        mergedArray.Add(list2[i]);
+                    }
+                }
+            }
+            else
+            {
+                for (int i = index1; i < end1; i++)
+                {
+                    if (!valueMap.ContainsKey(list1[i]))
+                    {
+                        mergedArray.Add(list1[i]);
+                    }
+                }
+
+            }
+
+            return mergedArray;
+        }
+
+        public void updateFrameScansRange( Dictionary<ushort, List<ushort>> newRangeValues)
+        {
+
+            foreach (ushort key in newRangeValues.Keys.ToList<ushort>())
+            {
+                if (frameAndScansRange.ContainsKey(key))
+                {
+                    //then we need to merge the two scan lists in sorted order
+                    List<ushort> prevSortedList = frameAndScansRange[key];
+                    List<ushort> newList = mergeSortUnique(prevSortedList, newRangeValues[key]);
+
+                    frameAndScansRange[key] = newList;
+
+                }
+                else
+                {
+                    //we simply add it to the dictionary
+                    frameAndScansRange.Add(key, newRangeValues[key]);
+                }
+            }
+
+
+        }
+
+
+        //checks if the given mass is within a tolerance of this feature
+        public bool containsMass(double massValue, int toleranceInPPM)
+        {
+                double differenceInPPM = Math.Abs(1000000 * (this.XValue - massValue) / this.XValue);
+
+                if (differenceInPPM <= toleranceInPPM)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            
+        }
+
+
+        public int containsPeak(MSPeak peak, ushort frameNum, ushort scanNum, ushort toleranceInPPM, ushort netRange, ushort driftRange )
+        {
+            if (peak == null)
+            {
+                return -1;
+            }
+            else
+            {
+
+                //TODO:: two peaks are the same if they are within a tolerance of each other in
+                //terms of mz, scan and lc frame. in this case we're only implementing mz values
+                //
+                double differenceInPPM = Math.Abs(1000000 * (peak.XValue - this.XValue) / this.XValue);
+
+                if (differenceInPPM <= toleranceInPPM)
+                {
+                    //it's within the mass tolerance of our peak
+                    //now check for net tolerance
+                    //check if the frameScansRange bit is set for the frame number and a few other scans within tolerance
+                    for (ushort i = (ushort)(frameNum - netRange); i < frameNum + netRange; i++)
+                    {
+                        if (frameAndScansRange.ContainsKey(i))
+                        {
+                            //then we have to check for the scans in that list and look for our scan values in that list
+                            //we have to keep that list sorted for fast searching
+                            List<ushort> scanList = frameAndScansRange[i];
+                            for (ushort j = (ushort)(scanNum - driftRange); j < scanNum + driftRange; j++)
+                            {
+                                if (scanList.BinarySearch(j) >= 0)
+                                {
+                                    //this means that the scan number is present in this list. 
+                                    //we're done here
+                                    return 0;
+                                }
+                            }
+                            
+                            //else we've searched through the entire scan list and didn't find 
+                            //the scan we're interested in.
+                            
+                        }
+                      
+                    }
+
+                    int netDiff = this.frameNumber.CompareTo(frameNum);
+                    if ( netDiff  == 0 ){
+                        //now compare on scan range
+                        return this.scanNumber.CompareTo( scanNumber );
+                    }
+                    else{
+                        return netDiff;
+                    }
+                }
+                else
+                {
+                    return peak.XValue.CompareTo(this.XValue);
+                }
+            }
+           }
+    }
+}
