e7c29cfc05c9ad28d0f66e4424c754ad2000bc50
Kaipo Tamura <Kaipo.Tamura@pnnl.gov>
2012-07-25 19:13:47 +0000

    Added TopDownTargetedResultToTextExporter and TopDownTargetedResultDTO

---
+++ DeconTools.Workflows/DeconTools.Workflows/Backend/FileIO/TopDownTargetedResultToTextExporter.cs
@@ -0,0 +1,94 @@
+﻿using System.Text;
+using DeconTools.Workflows.Backend.Results;
+
+namespace DeconTools.Workflows.Backend.FileIO
+{
+	public class TopDownTargetedResultToTextExporter : TargetedResultToTextExporter
+	{
+		public TopDownTargetedResultToTextExporter(string filename)
+            : base(filename)
+        {
+
+        }
+
+		#region Private Methods
+
+		protected override string addBasicTargetedResult(TargetedResultDTO result)
+		{
+			var topDownResult = (TopDownTargetedResultDTO)result;
+
+			var sb = new StringBuilder();
+
+			var prsmList = new StringBuilder();
+			foreach (var prsm in topDownResult.PrsmList)
+			{
+				if (prsmList.Length > 0) prsmList.Append(", ");
+				prsmList.Append(prsm);
+			}
+			var chargeList = new StringBuilder();
+			foreach (var charge in topDownResult.ChargeList)
+			{
+				if (chargeList.Length > 0) chargeList.Append(", ");
+				chargeList.Append(charge);
+			}
+
+			sb.Append(topDownResult.MatchedMassTagID);
+			sb.Append(Delimiter);
+			sb.Append(prsmList.ToString());
+			sb.Append(Delimiter);
+			sb.Append(chargeList.ToString());
+			sb.Append(Delimiter);
+			sb.Append(topDownResult.ProteinName);
+			sb.Append(Delimiter);
+			sb.Append(topDownResult.ProteinMass);
+			sb.Append(Delimiter);
+			sb.Append(topDownResult.PeptideSequence);
+			sb.Append(Delimiter);
+			sb.Append(topDownResult.EmpiricalFormula);
+			sb.Append(Delimiter);
+			sb.Append(topDownResult.Quantitation);
+			sb.Append(Delimiter);
+			sb.Append(topDownResult.ScanLC);
+			sb.Append(Delimiter);
+			sb.Append(topDownResult.FailureType);
+			sb.Append(Delimiter);
+			sb.Append(topDownResult.ErrorDescription);
+			
+			return sb.ToString();
+		}
+
+
+		protected override string buildHeaderLine()
+		{
+			var sb = new StringBuilder();
+
+			sb.Append("Prsm_ID");
+			sb.Append(Delimiter);
+			sb.Append("Prsm_list");
+			sb.Append(Delimiter);
+			sb.Append("Charge_list");
+			sb.Append(Delimiter);
+			sb.Append("Protein_name");
+			sb.Append(Delimiter);
+			sb.Append("Protein_mass");
+			sb.Append(Delimiter);
+			sb.Append("Peptide");
+			sb.Append(Delimiter);
+			sb.Append("Empirical_formula");
+			sb.Append(Delimiter);
+			sb.Append("Quantitation");
+			sb.Append(Delimiter);
+			sb.Append("Most_abundant_charge_apex_scan");
+			sb.Append(Delimiter);
+			sb.Append("Failure_type");
+			sb.Append(Delimiter);
+			sb.Append("Error_description");
+
+			return sb.ToString();
+
+		}
+
+		#endregion
+
+	}
+}
---
+++ DeconTools.Workflows/DeconTools.Workflows/Backend/Results/TopDownTargetedResultDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeconTools.Workflows.Backend.Results
+{
+	public class TopDownTargetedResultDTO : UnlabelledTargetedResultDTO
+	{
+		public List<int> PrsmList { get; set; }
+		public List<int> ChargeList { get; set; }
+		public float Quantitation { get; set; }
+
+		public string ProteinName { get; set; }
+		public double ProteinMass { get; set; }
+		public string PeptideSequence { get; set; }
+		public float ChromPeakSelectedHeight { get; set; }
+	}
+}
