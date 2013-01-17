17a2596ddf4886b72a59ae32f48d8c39beedb994
Scott Kronewitter <Scott.Kronewitter@pnnl.gov>
2013-01-17 19:04:23 +0000

    Updated MassTagFitScoreCalculatorLabeled so I can remove it.  Plan on transferring the improvements into MassTagFitScoreCalculator.  Default operation will remain unchanged as long as public IsotopicProfileType IsotopicProfileTarget { get; set; } is set to Unlabeled.
    Former-commit-id: 8311151bb51db53fa22b18e7c66582bd8a2ea41b
    Former-commit-id: deba23bf6d8b4af257498b0fac7e9c7fcdefd4e1

--- DeconTools.Backend/ProcessingTasks/FitScoreCalculators/MassTagFitScoreCalculatorLabeled.cs
+++ DeconTools.Backend/ProcessingTasks/FitScoreCalculators/MassTagFitScoreCalculatorLabeled.cs
@@ -7,14 +7,43 @@ namespace DeconTools.Backend.ProcessingTasks.FitScoreCalculators
 {
     public class MassTagFitScoreCalculatorLabeled:Task
     {
-     
+        /// <summary>
+        /// score experimental profile to labeled or unlabeled theoretical profile
+        /// </summary>
+        public IsotopicProfileType IsotopicProfileTarget { get; set; }
+
+        public MassTagFitScoreCalculatorLabeled()
+        {
+            IsotopicProfileTarget = IsotopicProfileType.LABELLED;
+        }
+
+        public MassTagFitScoreCalculatorLabeled(IsotopicProfileType lableType)
+        {
+            IsotopicProfileTarget = lableType;
+        }
 
         #region Public Methods
         public override void Execute(ResultCollection resultList)
         {
             Check.Require(resultList.Run.CurrentMassTag != null, this.Name + " failed; CurrentMassTag is empty");
-            Check.Require(resultList.Run.CurrentMassTag.IsotopicProfileLabelled != null, this.Name + " failed; Theor isotopic profile is empty. Run a TheorFeatureGenerator");
             Check.Require(resultList.Run.XYData != null && resultList.Run.XYData.Xvalues != null && resultList.Run.XYData.Xvalues.Length > 0, this.Name + " failed; Run's XY data is empty. Need to Run an MSGenerator");
+
+            IsotopicProfile theorProfile = new IsotopicProfile();
+            switch (IsotopicProfileTarget)
+            {
+                case IsotopicProfileType.UNLABELLED:
+                    Check.Require(resultList.Run.CurrentMassTag.IsotopicProfile != null, "Target's theoretical isotopic profile has not been established");
+                    theorProfile = resultList.Run.CurrentMassTag.IsotopicProfile;
+                    break;
+                case IsotopicProfileType.LABELLED:
+                    //Check.Require(resultList.Run.CurrentMassTag.IsotopicProfileLabelled != null, this.Name + " failed; Theor isotopic profile is empty. Run a TheorFeatureGenerator");
+                    Check.Require(resultList.Run.CurrentMassTag.IsotopicProfileLabelled != null, "Target's labelled theoretical isotopic profile has not been established");
+                    theorProfile = resultList.Run.CurrentMassTag.IsotopicProfileLabelled;
+                    break;
+                default:
+                    theorProfile = resultList.Run.CurrentMassTag.IsotopicProfile;
+                    break;
+            }
 
             TargetedResultBase result = resultList.CurrentTargetedResult;
             Check.Require(result != null, "No MassTagResult has been generated for CurrentMassTag");
@@ -25,7 +54,7 @@ namespace DeconTools.Backend.ProcessingTasks.FitScoreCalculators
                 return;
             }
 
-            IsotopicProfile theorProfile = resultList.Run.CurrentMassTag.IsotopicProfileLabelled;
+            //IsotopicProfile theorProfile = resultList.Run.CurrentMassTag.IsotopicProfileLabelled;
             int indexOfMostAbundantTheorPeak = theorProfile.GetIndexOfMostIntensePeak();
             int indexOfCorrespondingObservedPeak = PeakUtilities.getIndexOfClosestValue(result.IsotopicProfile.Peaklist,
                 theorProfile.getMostIntensePeak().XValue, 0, result.IsotopicProfile.Peaklist.Count - 1, 0.1);
