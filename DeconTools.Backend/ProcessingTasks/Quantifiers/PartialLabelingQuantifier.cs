21476860b4ef783ecdf91d4ba3842b3b4d37927e
Gordon Slysz <gordon.slysz@gmail.com>
2013-01-20 07:50:24 +0000

    Minor
    Former-commit-id: 11a6c41527254a0c1f95c44f52d889e7af68e81b
    Former-commit-id: 561eb66418e6e6180979b89376344e062157d66e

---
+++ DeconTools.Backend/ProcessingTasks/Quantifiers/PartialLabelingQuantifier.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DeconTools.Backend.Core;
+using DeconTools.Utilities;
+
+namespace DeconTools.Backend.ProcessingTasks.Quantifiers
+{
+    public class PartialLabelingQuantifier
+    {
+
+
+
+
+        public IsotopicProfile FindBestLabeledProfile(TargetBase target)
+        {
+            Check.Require(target!=null,"Target is null. You need a valid target in order to quantify it.");
+            
+
+
+
+
+
+
+
+        }
+
+
+
+
+    }
+}
