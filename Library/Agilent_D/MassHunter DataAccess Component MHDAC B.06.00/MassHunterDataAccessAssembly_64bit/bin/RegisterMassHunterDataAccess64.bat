967da29371dcae6b20c8083f3028db40f2fd1344
Gordon Slysz <gordon.slysz@gmail.com>
2013-01-30 00:40:06 +0000

    added new Agilent .dlls
    Former-commit-id: 55fd9a7e4a1d66e297d230ade68e682b86fe246b
    Former-commit-id: d7db5a1745c958755664397c80dafb1c6c3718ac

---
+++ Library/Agilent_D/MassHunter DataAccess Component MHDAC B.06.00/MassHunterDataAccessAssembly_64bit/bin/RegisterMassHunterDataAccess64.bat
@@ -0,0 +1,10 @@
+set REGASMPROG=%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319\regasm.exe
+
+%REGASMPROG% BaseCommon.dll /tlb /u
+%REGASMPROG% BaseDataAccess.dll /tlb /u
+%REGASMPROG% MassSpecDataReader.dll /tlb /u
+
+%REGASMPROG% BaseCommon.dll /tlb /codebase
+%REGASMPROG% BaseDataAccess.dll /tlb /codebase
+%REGASMPROG% MassSpecDataReader.dll /tlb /codebase
+
