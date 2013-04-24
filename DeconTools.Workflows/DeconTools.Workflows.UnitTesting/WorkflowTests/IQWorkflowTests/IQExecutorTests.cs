2f4cff0c02d556f98a29e4d71eaec2072f4ef324
Gordon Slysz <gordon.slysz@gmail.com>
2013-04-24 00:06:30 +0000

    added another IQExecutorTest
    Former-commit-id: 7d9ef84b92d90752fc05214143322cc599b57ff8
    Former-commit-id: 479a0dd00569868d63233d97deb5845dd026c10c

--- DeconTools.Workflows/DeconTools.Workflows.UnitTesting/WorkflowTests/IQWorkflowTests/IQExecutorTests.cs
+++ DeconTools.Workflows/DeconTools.Workflows.UnitTesting/WorkflowTests/IQWorkflowTests/IQExecutorTests.cs
@@ -47,6 +47,9 @@ namespace DeconTools.Workflows.UnitTesting.WorkflowTests.IQWorkflowTests
             executorBaseParameters.ChromGenSourceDataSigNoise = 2;
             executorBaseParameters.ResultsFolder = resultsFolder;
             executorBaseParameters.TargetsFilePath = targetsFile;
+            
+            
+            //create no more than two charge state targets per peptide
             executorBaseParameters.MaxNumberOfChargeStateTargetsToCreate = 2;
 
             var executor = new IqExecutor(executorBaseParameters);
@@ -65,6 +68,96 @@ namespace DeconTools.Workflows.UnitTesting.WorkflowTests.IQWorkflowTests
             }
 
           
+        }
+
+
+        //"\\protoapps\UserData\Slysz\Standard_Testing\Targeted_FeatureFinding\Unlabelled\Targets\QCShew_Formic_MassTags_Bin10_singleTest1.txt"
+
+        [Category("MustPass")]
+        [Test]
+        public void ExecutorIqTest1()
+        {
+            var util = new IqTargetUtilities();
+            string testFile = UnitTesting2.FileRefs.RawDataMSFiles.OrbitrapStdFile1;
+            string peaksTestFile = @"\\protoapps\UserData\Slysz\DeconTools_TestFiles\QC_Shew_08_04-pt5-2_11Jan09_Sphinx_08-11-18_scans5500-6500_peaks.txt";
+
+            string targetsFile =
+                @"\\protoapps\UserData\Slysz\Standard_Testing\Targeted_FeatureFinding\Unlabelled\Targets\QCShew_Formic_MassTags_Bin10_singleTest1.txt";
+
+            string resultsFolder = @"\\protoapps\UserData\Slysz\Standard_Testing\Targeted_FeatureFinding\Unlabelled\Results";
+
+            string expectedResultsFilename = resultsFolder + "\\" + RunUtilities.GetDatasetName(testFile) + "_iqResults.txt";
+            if (File.Exists(expectedResultsFilename)) File.Delete(expectedResultsFilename);
+
+
+            WorkflowExecutorBaseParameters executorBaseParameters = new BasicTargetedWorkflowExecutorParameters();
+            executorBaseParameters.ChromGenSourceDataPeakBR = 3;
+            executorBaseParameters.ChromGenSourceDataSigNoise = 2;
+            executorBaseParameters.ResultsFolder = resultsFolder;
+            executorBaseParameters.TargetsFilePath = targetsFile;
+            executorBaseParameters.MaxMzForDefiningChargeStateTargets = 2000;
+
+
+            //create no more than two charge state targets per peptide
+            //executorBaseParameters.MaxNumberOfChargeStateTargetsToCreate = 2;
+
+            var executor = new IqExecutor(executorBaseParameters);
+            executor.ChromSourceDataFilePath = peaksTestFile;
+
+            executor.LoadAndInitializeTargets(targetsFile);
+            
+
+            foreach (var iqTarget in executor.Targets)
+            {
+                int numChildTargets = iqTarget.GetChildCount();
+
+                
+                Console.WriteLine(iqTarget + "\t" + numChildTargets);
+            }
+
+
+            Run run = new RunFactory().CreateRun(testFile);
+            executor.SetRun(run);
+
+            var targetedWorkflowParameters = new BasicTargetedWorkflowParameters();
+            targetedWorkflowParameters.ChromNETTolerance = 0.5;
+
+            //define workflows for parentTarget and childTargets
+            var parentWorkflow = new BasicIqWorkflow(run, targetedWorkflowParameters);
+            var childWorkflow = new BasicIqWorkflow(run, targetedWorkflowParameters);
+
+            IqWorkflowAssigner workflowAssigner = new IqWorkflowAssigner();
+            workflowAssigner.AssignWorkflowToParent(parentWorkflow, executor.Targets);
+            workflowAssigner.AssignWorkflowToChildren(childWorkflow, executor.Targets);
+
+            //Main line for executing IQ:
+            executor.Execute();
+
+            //Test the results...
+            Assert.IsTrue(File.Exists(expectedResultsFilename), "results file doesn't exist");
+            int numResultsInResultsFile = 0;
+            bool outputToConsole = true;
+
+            using (StreamReader reader = new StreamReader(expectedResultsFilename))
+            {
+                while (reader.Peek() != -1)
+                {
+                    string line = reader.ReadLine();
+                    numResultsInResultsFile++;
+
+                    if (outputToConsole)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+
+            Assert.IsTrue(numResultsInResultsFile > 1, "No results in output file");
+
+            //the results in the Executor are in the a Result tree. So there should be just 10. 
+            Assert.AreEqual(1, executor.Results.Count);
+
+
         }
 
 
