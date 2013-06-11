fb837ead4c87353d8addf81cfb7c0a743d0aa0ac
Gordon Slysz <gordon.slysz@gmail.com>
2013-06-11 17:41:44 +0000

    minor svn-related
    Former-commit-id: f9dae2671969c84e379c5e2cb2ff907859ee6523
    Former-commit-id: 60097e664d7eba9c9f9911e41a310ed827de8c9b

--- TestConsole1/Program.cs
+++ TestConsole1/Program.cs
@@ -54,30 +54,7 @@ namespace TestConsole1
         public static void TestErnestosData1()
         {
 
-            string baseFolder = @"\\protoapps\DataPkgs\Public\2012\573_O16O18_Data_analysis_of_ubiquintated_peptides";
-
-            string outputFolder = @"D:\Temp\TargetedTesting";
-
-            BasicTargetedWorkflowExecutorParameters executorParameters = new BasicTargetedWorkflowExecutorParameters();
-            executorParameters.TargetsBaseFolder = baseFolder + Path.DirectorySeparatorChar + "Targets";
-            executorParameters.WorkflowParameterFile = baseFolder + Path.DirectorySeparatorChar + "Parameters" + Path.DirectorySeparatorChar + "O16O18WorkflowParameters_2011_08_23_sum5.xml";
-
-            executorParameters.TargetType = Globals.TargetType.LcmsFeature;
-            executorParameters.ResultsFolder = outputFolder + Path.DirectorySeparatorChar + "Results\\Testing";
-            executorParameters.LoggingFolder = outputFolder + Path.DirectorySeparatorChar + "Logs";
-
-            string testDatasetPath = @"\\protoapps\UserData\Slysz\Data\O16O18\Ernesto\PSI_LRW_18O_02A_18Jun12_Falcon_12-03-34.RAW";
-            testDatasetPath = @"D:\Data\O16O18\Ernesto\PSI_LRW_1to1_03A_6Jul12_Falcon_12-06-04.raw";
-
-            string outputtedParameterFile =
-                @"\\protoapps\DataPkgs\Public\2012\573_O16O18_Data_analysis_of_ubiquintated_peptides\Parameters\ExecutorParameters1.xml";
-
-            //executorParameters.SaveParametersToXML(outputtedParameterFile);
-
-
-            TargetedWorkflowExecutor executor = new BasicTargetedWorkflowExecutor(executorParameters, testDatasetPath);
-            executor.Execute();
-
+         
 
         }
 
@@ -92,8 +69,7 @@ namespace TestConsole1
             var executorParameters = new TopDownTargetedWorkflowExecutorParameters();
             executorParameters.CopyRawFileLocal = true;
             executorParameters.FolderPathForCopiedRawDataset = copyToFolder;
-            executorParameters.LoggingFolder = outputFolder;
-            executorParameters.ResultsFolder = outputFolder;
+            
             executorParameters.TargetType = Globals.TargetType.LcmsFeature;
             executorParameters.WorkflowParameterFile =
                 @"\\proto-7\VOrbiETD01\2012_1\CPTAC_Peptidome_Test1_P1_Poroshell_03Feb12_Frodo_Poroshell300SB\MSA201202231748_Auto796395\MSAlign_Quant_Workflow_2012-07-25.xml";
