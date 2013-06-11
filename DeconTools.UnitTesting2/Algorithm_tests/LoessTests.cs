ae14d8cf52ade673203f9e9b165765729bad88b8
Gordon Slysz <gordon.slysz@gmail.com>
2013-06-11 17:38:22 +0000

    added more LoessTests
    Former-commit-id: 41da4ba1823face4d94921fd09a1c7af021cea39
    Former-commit-id: 9c8a5acebf74bbe95c5e62068aa3e061bcc4a1d2

--- DeconTools.UnitTesting2/Algorithm_tests/LoessTests.cs
+++ DeconTools.UnitTesting2/Algorithm_tests/LoessTests.cs
@@ -12,7 +12,7 @@ namespace DeconTools.UnitTesting2.Algorithm_tests
     public class LoessTests
     {
         [Test]
-        public void Test1()
+        public void LoessOnMassAlignmentData1()
         {
 
             string testFile = @"\\protoapps\UserData\Slysz\DeconTools_TestFiles\MassAlignmentTesting\scanPpmErrorValues1.txt";
@@ -30,6 +30,103 @@ namespace DeconTools.UnitTesting2.Algorithm_tests
                 Console.WriteLine(xydata.Xvalues[i] +"\t"+  xydata.Yvalues[i] +  "\t" + smoothedData[i]);
             }
         }
+
+
+        [Test]
+        public void LoessOnNETAlignmentData1()
+        {
+
+            string testFile = @"\\protoapps\UserData\Slysz\DeconTools_TestFiles\MassAlignmentTesting\scanToNetTheorData1.txt";
+
+            XYData xydata = TestUtilities.LoadXYDataFromFile(testFile);
+
+            Dictionary<decimal, double> scanNetVals = new Dictionary<decimal, double>();
+
+            for (int i = 0; i < xydata.Xvalues.Length; i++)
+            {
+                decimal xval =  (decimal) Math.Round(xydata.Xvalues[i],1);
+                double yval = xydata.Yvalues[i];
+
+                if (!scanNetVals.ContainsKey(xval))
+                {
+                    scanNetVals.Add(xval, yval);
+                }
+                
+            
+            }
+
+            XYData xyDataForInterpolator = new XYData();
+            xyDataForInterpolator.Xvalues = scanNetVals.Keys.Select(p => (double) p).ToArray();
+            xyDataForInterpolator.Yvalues = scanNetVals.Values.ToArray();
+
+            double bandwidth = 0.005;
+            int robustIter = 4;
+            LoessInterpolator loessInterpolator = new LoessInterpolator(bandwidth, robustIter);
+            
+
+            var smoothedData = loessInterpolator.Smooth(xyDataForInterpolator.Xvalues, xyDataForInterpolator.Yvalues);
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < xyDataForInterpolator.Xvalues.Length; i++)
+            {
+                sb.Append(xyDataForInterpolator.Xvalues[i] + "\t" + xyDataForInterpolator.Yvalues[i] + "\t" + smoothedData[i]);
+                sb.Append(Environment.NewLine);
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+
+
+
+        [Test]
+        public void LoessOnNETAlignmentData2()
+        {
+
+            string testFile =
+                @"C:\Users\d3x720\Documents\PNNL\My_DataAnalysis\2013\IQ\2013_06_04_Using MSGF for IQ alignment - part2\QCShew_ScanVsTheorNetValues.txt";
+
+            XYData xydata = TestUtilities.LoadXYDataFromFile(testFile);
+
+            Dictionary<decimal, double> scanNetVals = new Dictionary<decimal, double>();
+
+            for (int i = 0; i < xydata.Xvalues.Length; i++)
+            {
+                decimal xval = (decimal)Math.Round(xydata.Xvalues[i], 1);
+                double yval = xydata.Yvalues[i];
+
+                if (!scanNetVals.ContainsKey(xval))
+                {
+                    scanNetVals.Add(xval, yval);
+                }
+
+
+            }
+
+            XYData xyDataForInterpolator = new XYData();
+            xyDataForInterpolator.Xvalues = scanNetVals.Keys.Select(p => (double)p).ToArray();
+            xyDataForInterpolator.Yvalues = scanNetVals.Values.ToArray();
+
+            double bandwidth = 0.1;
+            int robustIter = 2;
+            LoessInterpolator loessInterpolator = new LoessInterpolator(bandwidth, robustIter);
+
+
+            var smoothedData = loessInterpolator.Smooth(xyDataForInterpolator.Xvalues, xyDataForInterpolator.Yvalues);
+
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < xyDataForInterpolator.Xvalues.Length; i++)
+            {
+                sb.Append(xyDataForInterpolator.Xvalues[i] + "\t" + xyDataForInterpolator.Yvalues[i] + "\t" + smoothedData[i]);
+                sb.Append(Environment.NewLine);
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+
+
+
 
     }
 }
