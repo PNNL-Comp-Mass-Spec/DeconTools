0867c69f18f8e7910d1e6ac87c6590f28f91fb96
Grant Fujimoto <grant.fujimoto@pnnl.gov>
2013-11-26 22:11:28 +0000

    Included manifest files for SxS errors occuring in DeconTools.Backend
    Former-commit-id: 92a61fb5a42f19d7d1fe45f565b6603ad086d877
    Former-commit-id: b43e09c143be0b569097a51a15d0f97aedd8ad98

---
+++ DeconTools.Workflows/DeconTools.Workflows.UnitTesting/IqUnitTesting/BottomUpIqTesting.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using DeconTools.Backend.Core;
+using DeconTools.Backend.Runs;
+using DeconTools.Backend.Utilities;
+using DeconTools.Workflows.Backend.Core;
+using DeconTools.Workflows.Backend.Utilities;
+using NUnit.Framework;
+
+namespace DeconTools.Workflows.UnitTesting.IqUnitTesting
+{
+	public class BottomUpIqTesting
+	{
+
+		[Test]
+		public void ExecutorTest1()
+		{
+			Console.WriteLine(Environment.CurrentDirectory);
+			var util = new IqTargetUtilities();
+			string testFile = @"\\proto-5\External_Waters_TOF_Xfer\MzML_Files\130716_iPRG14_004.mzML";
+			//string peaksTestFile =
+				//@"\\protoapps\UserData\Slysz\DeconTools_TestFiles\QC_Shew_08_04-pt5-2_11Jan09_Sphinx_08-11-18_scans5500-6500_peaks.txt";
+			string targetsFile = @"\\protoapps\UserData\Fujimoto\SangtaeBottomUp\msgfPlus\C~~data~iPRG 2014~130716_iPRG14_004.raw.-1_Filtered.tsv";
+
+			WorkflowExecutorBaseParameters executorBaseParameters = new BasicTargetedWorkflowExecutorParameters();
+			executorBaseParameters.ChromGenSourceDataPeakBR = 3;
+			executorBaseParameters.ChromGenSourceDataSigNoise = 2;
+			executorBaseParameters.TargetsFilePath = targetsFile;
+			executorBaseParameters.OutputFolderBase =
+				@"\\protoapps\UserData\Fujimoto\SangtaeBottomUp\Results";
+
+
+			string expectedResultsFilename = executorBaseParameters.OutputFolderBase + "\\IqResults\\" +
+			                                 RunUtilities.GetDatasetName(testFile) + "_iqResults.txt";
+			if (File.Exists(expectedResultsFilename)) File.Delete(expectedResultsFilename);
+
+
+			Run run = new RunFactory().CreateRun(testFile);
+
+			var executor = new IqExecutor(executorBaseParameters, run);
+			//executor.ChromSourceDataFilePath = peaksTestFile;
+
+			executor.LoadAndInitializeTargets(targetsFile);
+
+			var targetedWorkflowParameters = new BasicTargetedWorkflowParameters();
+			targetedWorkflowParameters.ChromNETTolerance = 0.05;
+
+			//define workflows for parentTarget and childTargets
+			var parentWorkflow = new ChromPeakDeciderIqWorkflow(run, targetedWorkflowParameters);
+			var childWorkflow = new ChargeStateChildIqWorkflow(run, targetedWorkflowParameters);
+
+			IqWorkflowAssigner workflowAssigner = new IqWorkflowAssigner();
+			workflowAssigner.AssignWorkflowToParent(parentWorkflow, executor.Targets);
+			workflowAssigner.AssignWorkflowToChildren(childWorkflow, executor.Targets);
+
+			//SipperDataDump.DataDumpSetup(@"\\pnl\projects\MSSHARE\Merkley_Eric\For_Grant\IqResults\EXP6B_F1_CSCL_LIGHT_130520020056\EXP6B_F1_CSCL_LIGHT_FULLRESULTS.txt");
+
+			//Main line for executing IQ:
+			executor.Execute();
+
+			//Test the results...
+			int numResultsInResultsFile = 0;
+			bool outputToConsole = true;
+
+			using (StreamReader reader = new StreamReader(expectedResultsFilename))
+			{
+				while (reader.Peek() != -1)
+				{
+					string line = reader.ReadLine();
+					numResultsInResultsFile++;
+
+					if (outputToConsole)
+					{
+						Console.WriteLine(line);
+					}
+				}
+			}
+		}
+
+	}
+}
--- DeconTools.Workflows/DeconTools.Workflows.UnitTesting/IqUnitTesting/TopDownIqTesting.cs
+++ DeconTools.Workflows/DeconTools.Workflows.UnitTesting/IqUnitTesting/TopDownIqTesting.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Text;
 using DeconTools.Backend.Core;
 using DeconTools.Backend.Runs;
+using DeconTools.Backend.Utilities;
+using DeconTools.Backend.Utilities.IsotopeDistributionCalculation;
 using DeconTools.Workflows.Backend.Core;
 using NUnit.Framework;
 
@@ -37,8 +39,11 @@ namespace DeconTools.Workflows.UnitTesting.IqUnitTesting
 
 			var executor = new TopDownMSAlignExecutor(executorBaseParameters, run);
 
-
+			var s = Stopwatch.StartNew();
 			executor.LoadAndInitializeTargets(targetsFile);
+			s.Stop();
+			var rt = s.Elapsed;
+			Console.WriteLine("import: " + rt);
 
 			var targetedWorkflowParameters = new BasicTargetedWorkflowParameters();
 			targetedWorkflowParameters.ChromNETTolerance = 0.05;
@@ -65,6 +70,42 @@ namespace DeconTools.Workflows.UnitTesting.IqUnitTesting
 			var runtime = stopwatch.Elapsed;
 			Console.WriteLine("Runtime: " + runtime);
 		}
+
+
+		[Test]
+		public void Get3DElutionAndExportToFileTest1()
+		{
+			string rawFile = @"\\protoapps\UserData\Fujimoto\TopDownPaperData\FINAL_DATA\_004\SBEP_STM_004_02272012_Aragon.raw";
+			string peaksFile = @"\\protoapps\UserData\Fujimoto\TopDownPaperData\FINAL_DATA\_004\SBEP_STM_004_02272012_Aragon_peaks.txt";
+
+			var run = RunUtilities.CreateAndLoadPeaks(rawFile, peaksFile);
+			//var run = new RunFactory().CreateRun(rawFile);
+
+			string outputFile = @"\\protoapps\UserData\Fujimoto\TopDownPaperData\FINAL_DATA\_004\3D_PLOT\3DelutionProfile.txt";
+
+
+			Assert.IsNotNull(run);
+			Assert.IsTrue(run.ResultCollection.MSPeakResultList.Count > 0);
+
+			var extractor = new IsotopicProfileElutionExtractor();
+
+			int minScan = 3270;
+			int maxScan = 3350;
+			double minMZ = 700;
+			double maxMZ = 1700;
+
+			int[] scans;
+			float[] intensities;
+			double[] mzBinVals;
+
+
+			extractor.Get3DElutionProfileFromPeakLevelData(run, minScan, maxScan, minMZ, maxMZ, out scans, out mzBinVals, out intensities);
+
+			var intensities2D = extractor.GetIntensitiesAs2DArray();
+			extractor.OutputElutionProfileToFile(outputFile);
+
+		}
+
 
 	}
 }
