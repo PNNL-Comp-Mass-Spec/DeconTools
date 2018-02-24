e9ef4577cb3ca2dae037c8343e2f9f91c5a019b3
alchemistmatt <matthew.monroe@pnnl.gov>
2018-02-23 18:51:42 -0800

    Update IqConsoleOptions to use the Option tags supported by CommandLineParser in Prism.dll

--- DeconTools.Workflows/TargetedWorkflowConsole/IqConsoleOptions.cs
+++ DeconTools.Workflows/TargetedWorkflowConsole/IqConsoleOptions.cs
@@ -1,42 +1,62 @@
 ﻿using System.Reflection;
-using CommandLine;
-using CommandLine.Text;
+using PRISM;
 
 namespace IQ.Console
 {
     public class IqConsoleOptions
     {
-        [ParserState]
-        public IParserState LastParserState { get; set; }
+        private const string PROGRAM_DATE = "February 23, 2018";
 
-        [Option('i', "InputFile", Required = true, HelpText = "Input dataset or a text file containing a list of datasets. Supported file formats = Thermo .raw; ")]
+        public IqConsoleOptions()
+        {
+            InputFile = string.Empty;
+            TargetsFile = string.Empty;
+            ReferenceTargetFile = string.Empty;
+            TargetFileForAlignment = string.Empty;
+            OutputFolder = string.Empty;
+            UseInputScan = false;
+            WorkflowParameterFile = string.Empty;
+            TemporaryWorkingFolder = string.Empty;
+            IsAlignmentPerformed = false;
+            AlignmentParameterFile = string.Empty;
+            IsMassAlignmentPerformed = false;
+            IsNetAlignmentPerformed = false;
+            ChromGenSourceDataPeakBr = 2;
+            ChromGenSourceDataSigNoise = 2;
+            DataIsThresholded = true;
+            ChromGenSourceDataProcessMsMs = false;
+            UseOldIq = false;
+            OutputGraphs = false;
+        }
+
+        [Option("I", "InputFile", ArgPosition = 1, Required = true, HelpText = "Input dataset or a text file containing a list of datasets. Can be a path to a thermo .Raw file, or a DMS dataset name")]
         public string InputFile { get; set; }
 
-        [Option('t', "TargetsFile", DefaultValue = "", HelpText = "A text file containing a list of IQ targets. Supported formats= .txt, .mgf")]
-        public string TargetFile { get; set; }
+        [Option("T", "TargetsFile", ArgPosition = 3, HelpText = "A text file containing a list of IQ targets (overrides targets file defined in the WorkflowParameterFile .xml file). Supported formats= .txt, .mgf")]
+        public string TargetsFile { get; set; }
 
-        [Option('r', "ReferenceFile", DefaultValue = "", Required = false, HelpText = "A text file containing NET alignment reference data. Supported formats= .txt")]
+        [Option("R", "ReferenceFile", HelpText = "A text file containing NET alignment reference data. Supported formats= .txt")]
         public string ReferenceTargetFile { get; set; }
 
-        [Option('a', "AlignmentTargetsFile", DefaultValue = "", HelpText = "A text file containing a list of IQ targets which are used in calibrating mass and elution time. Supported formats= .txt, .mgf")]
+        [Option("A", "AlignmentTargetsFile", HelpText = "A text file containing a list of IQ targets which are used in calibrating mass and elution time. Supported formats= .txt, .mgf")]
         public string TargetFileForAlignment { get; set; }
 
-        [Option('d', "OutputFolder", DefaultValue = "", HelpText = "Output folder ")]
+        [Option("D", "OutputFolder", HelpText = "Output folder ")]
         public string OutputFolder { get; set; }
 
         [Option("UseInputScan", HelpText = "Determines whether or not the user-supplied LC scan is used during IQ processing.")]
         public bool UseInputScan { get; set; }
 
-        [Option('w', "WorkflowParameterFile", DefaultValue = "",  HelpText = "An .xml file containing a list of IQ parameters")]
+        [Option("W", "WorkflowParameterFile", ArgPosition = 2, HelpText = "An .xml file containing a list of IQ parameters")]
         public string WorkflowParameterFile { get; set; }
 
-        [Option('f', "TemporaryWorkingFolder", DefaultValue = "", HelpText = "Temporary working folder where dataset is copied")]
+        [Option("F", "TemporaryWorkingFolder", HelpText = "Temporary working folder where dataset is copied")]
         public string TemporaryWorkingFolder { get; set; }
 
-        [Option('q', "IsAlignmentPerformed", HelpText = "If true, IQ alignment is performed using AlignmentTargetsFile.")]
+        [Option("Q", "IsAlignmentPerformed", HelpText = "If true, IQ alignment is performed using AlignmentTargetsFile.  This option is obsolete; use IsMassAlignmentPerformed instead")]
         public bool IsAlignmentPerformed { get; set; }
 
-        [Option('e', "AlignmentParameterFile", DefaultValue = "", HelpText = "Parameters used for IQ mass and NET alignment")]
+        [Option("E", "AlignmentParameterFile", HelpText = "Parameters used for IQ mass and NET alignment")]
         public string AlignmentParameterFile { get; set; }
 
         [Option("DoMassAlignment", HelpText = "If true, mass alignment is performed.")]
@@ -45,13 +65,13 @@ namespace IQ.Console
         [Option("DoNetAlignment", HelpText = "If true, NET alignment is performed.")]
         public bool IsNetAlignmentPerformed { get; set; }
 
-        [Option("PeakBRForChromGenerator", DefaultValue = 2d, HelpText = "Peak detector setting. Typical value ranges from 1 - 10, with lower values creating more peaks on which XIC data is based.")]
+        [Option("PeakBRForChromGenerator", HelpText = "Peak detector setting. Typical value ranges from 1 - 10, with lower values creating more peaks on which XIC data is based.")]
         public double ChromGenSourceDataPeakBr { get; set; }
 
-        [Option('s', "SignalToNoiseThreshForChromGenerator", DefaultValue = 2d, HelpText = "Peak detector setting. Typical value ranges from 1 - 10, with lower values creating more peaks on which XIC data is based.")]
+        [Option("S", "SignalToNoiseThreshForChromGenerator", HelpText = "Peak detector setting. Typical value ranges from 1 - 10, with lower values creating more peaks on which XIC data is based.")]
         public double ChromGenSourceDataSigNoise { get; set; }
 
-        [Option('h', "Thresholded", DefaultValue = true, HelpText = "Some mass spec data, like Orbitrap data, is thresholded. If so, set this to true.")]
+        [Option("H", "Thresholded", HelpText = "Some mass spec data, like Orbitrap data, is thresholded. If so, set this to true.")]
         public bool DataIsThresholded { get; set; }
 
         [Option("ChromGenSourceDataProcessMsMs", HelpText = "Some mass spec data, like Orbitrap data, is thresholded. If so, set this to true.")]
