34ea88e9e7b1ef4d085ad16a968d8781c046830d
alchemistmatt <matthew.monroe@pnnl.gov>
2019-07-26 17:16:28 -0700

    Update program date; now using System.Data.SQLite 1.0.111

--- DeconConsole/Program.cs
+++ DeconConsole/Program.cs
@@ -7,7 +7,7 @@ namespace DeconConsole
     public class Program
     {
 
-        public const string PROGRAM_DATE = "July 16, 2019";
+        public const string PROGRAM_DATE = "July 26, 2019";
 
         public static int Main(string[] args)
         {
