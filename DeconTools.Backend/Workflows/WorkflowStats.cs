d0fb2466e8b0f1fc4daf01c44f5f4ff5aa903550
alchemistmatt <matthew.monroe@pnnl.gov>
2020-03-02 18:23:02 -0800

    Add comments and remove unused property

--- DeconTools.Backend/Workflows/WorkflowStats.cs
+++ DeconTools.Backend/Workflows/WorkflowStats.cs
@@ -4,12 +4,36 @@ namespace DeconTools.Backend.Workflows
 {
     public class WorkflowStats
     {
+        /// <summary>
+        /// Number of deisotoped features
+        /// </summary>
         public int NumFeatures;
+
+        /// <summary>
+        /// Number of peaks found
+        /// </summary>
         public int NumPeaks;
+
+        /// <summary>
+        /// Number of scans processed
+        /// </summary>
+        /// <remarks>For UIMF files, this is not the number of frames; it is the number of scans deisotoped</remarks>
+        public int NumScans;
+
+        /// <summary>
+        /// Start time
+        /// </summary>
         public DateTime TimeStarted;
+
+        /// <summary>
+        /// Finish time
+        /// </summary>
         public DateTime TimeFinished;
 
+        /// <summary>
+        /// Elapsed time
+        /// </summary>
         public TimeSpan ElapsedTime => TimeFinished - TimeStarted;
-        public int ElapsedSeconds => (int)ElapsedTime.TotalSeconds;
+
     }
 }
