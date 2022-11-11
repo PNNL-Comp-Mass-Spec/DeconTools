e29060817bfbdad7235e4ceb5ccbbecdb4b763e7
alchemistmatt <matthew.monroe@pnnl.gov>
2022-11-10 16:00:17 -0800

    Update distribution batch file

--- Bin/Distribute_Files_Work.bat
+++ Bin/Distribute_Files_Work.bat
@@ -9,10 +9,14 @@ echo Copying files to %1
 xcopy ..\DeconConsole\bin\x64\Release\*.exe                   %1 /D /Y /F
 xcopy ..\DeconConsole\bin\x64\Release\*.dll                   %1 /D /Y /F
 
-xcopy ..\DeconConsole\bin\x64\Release\ThermoRawFileReader.pdb %1 /D /Y /F
 xcopy ..\DeconConsole\bin\x64\Release\DeconTools.Backend.pdb  %1 /D /Y /F
 xcopy ..\DeconConsole\bin\x64\Release\DeconConsole.pdb        %1 /D /Y /F
-xcopy ..\DeconConsole\bin\x64\Release\ThermoRawFileReader.xml %1 /D /Y /F
+
+if exist ..\DeconConsole\bin\x64\Release\ProteowizardWrapper.pdb xcopy ..\DeconConsole\bin\x64\Release\ProteowizardWrapper.pdb %1 /D /Y /F
+if exist ..\DeconConsole\bin\x64\Release\ProteowizardWrapper.xml xcopy ..\DeconConsole\bin\x64\Release\ProteowizardWrapper.xml %1 /D /Y /F
+
+if exist ..\DeconConsole\bin\x64\Release\ThermoRawFileReader.pdb xcopy ..\DeconConsole\bin\x64\Release\ThermoRawFileReader.pdb %1 /D /Y /F
+if exist ..\DeconConsole\bin\x64\Release\ThermoRawFileReader.xml xcopy ..\DeconConsole\bin\x64\Release\ThermoRawFileReader.xml %1 /D /Y /F
 
 xcopy ..\DeconConsole\bin\x64\Release\x64\SQLite.Interop.dll  %1 /D /Y /F
 xcopy ..\DeconConsole\bin\x64\Release\x86\SQLite.Interop.dll  %1 /D /Y /F
